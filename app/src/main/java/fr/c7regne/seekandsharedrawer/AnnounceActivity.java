51efaf8b8aba1c06f809fcce5b25acab9289bfe5
Corentin Régné <corentin.regne@gmail.com>
2020-04-23 19:20:06 +0200

    Second push, best writing in database with good increment, better navigation

---
+++ app/src/main/java/fr/c7regne/seekandsharedrawer/AnnounceActivity.java
@@ -0,0 +1,6 @@
+package fr.c7regne.seekandsharedrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnnounceActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/fr/c7regne/seekandsharedrawer/FavoriteActivity.java
@@ -0,0 +1,17 @@
+package fr.c7regne.seekandsharedrawer;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnnounceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_announce);
+    }
+}
