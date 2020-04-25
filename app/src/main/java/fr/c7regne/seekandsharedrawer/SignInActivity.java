b755ecdf922e1e06f7dd94ef50bf7b7a6a0cbf7d
TanBoi <tanguy.boittin@gmail.com>
2020-04-25 18:58:56 +0200

    chgment time de descente nuages

--- app/src/main/java/fr/c7regne/seekandsharedrawer/SignInActivity.java
+++ app/src/main/java/fr/c7regne/seekandsharedrawer/SignInActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
