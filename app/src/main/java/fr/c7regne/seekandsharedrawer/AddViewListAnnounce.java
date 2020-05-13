39f0586d24c7b126a2be75c8cb2455f649fa8e32
Lenael Jagline <lenaeldu35@gmail.com>
2020-05-13 09:21:09 +0200

    recuperation de v13

--- app/src/main/java/fr/c7regne/seekandsharedrawer/AddViewListAnnounce.java
+++ app/src/main/java/fr/c7regne/seekandsharedrawer/AddViewListAnnounce.java
@@ -11,6 +11,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ProgressBar;
 
 
 import androidx.annotation.RequiresApi;
@@ -22,13 +23,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 
+
 public class AddViewListAnnounce extends AppCompatActivity {
 
-
+    private ProgressBar spinner;
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @SuppressLint({"ResourceAsColor", "SetTextI18n"})
     public LinearLayout addAnnounceUser(final Context activity, String Title, String Date, String DP, String SP, String Content, String UserName, String UserId) {
-
 
         //linearlayout à empiler
         LinearLayout newLL = new LinearLayout(activity);
--- app/src/main/java/fr/c7regne/seekandsharedrawer/HomeFragment.java
+++ app/src/main/java/fr/c7regne/seekandsharedrawer/HomeFragment.java
@@ -1,13 +1,18 @@
 package fr.c7regne.seekandsharedrawer;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,25 +26,77 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class HomeFragment extends Fragment {
 
 
     public static final String EXTRA_ID="fr.c7regne.seekandsharedrawer";
 
+
     private Button search;
     private Button publication;
     DatabaseReference reff;
     String currentUserId;
+    private View v;
+
+    int pStatus = 0;
+    private Handler handler = new Handler();
+    TextView tv;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View v2 = inflater.inflate(R.layout.fragment_home, container, false);
-
         reff = FirebaseDatabase.getInstance().getReference().child("Tanguy");
         DatabaseReference[] tabReff = Function.Parcours();
+/*
+        //circle
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.circular);
+        final ProgressBar mProgress=v.findViewById(R.id.circularProgressbar);
+        mProgress.setProgress(0);   // Main Progress
+        mProgress.setSecondaryProgress(100); // Secondary Progress
+        mProgress.setMax(100); // Maximum Progress
+        mProgress.setProgressDrawable(drawable);
+
+      /*  ObjectAnimator animation = ObjectAnimator.ofInt(mProgress, "progress", 0, 100);
+        animation.setDuration(50000);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (pStatus < 100) {
+                    pStatus += 1;
+
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            mProgress.setProgress(pStatus);
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        // Just to display the progress slowly
+                        Thread.sleep(8); //thread will take approx 1.5 seconds to finish
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();*/
+
+
+
 
         for (DatabaseReference data : tabReff) {
         data.addValueEventListener(new ValueEventListener() {
@@ -70,10 +127,11 @@ public class HomeFragment extends Fragment {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
+
         });}
 
         return v2;
     }
+
 }
